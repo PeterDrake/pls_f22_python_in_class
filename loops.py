51166e6f98349aef840fea58ad9980e732e0ca40
Peter Drake <drake@lclark.edu>
2022-11-09 10:40:27 -0800

    Loops

---
+++ loops.py
@@ -0,0 +1,27 @@
+x = 0
+while True:
+    print(x)
+    x += 1
+    if x >= 3:
+        break
+
+
+sum = 0
+for i in range(1000):
+    sum += i
+print(sum)
+
+
+def numbers():
+    x = 0
+    while True:
+        x += 1
+        yield x
+
+for i in numbers():
+    print(i)
+    if i >= 10:
+        break
+
+print([x for x in numbers() if x < 10])
+
