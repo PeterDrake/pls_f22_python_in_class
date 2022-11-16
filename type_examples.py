fffa2c6443dd78e31e1bceefe4ba5f681231b3c7
Peter Drake <drake@lclark.edu>
2022-11-16 15:57:41 -0800

    type examples

---
+++ type_examples.py
@@ -0,0 +1,15 @@
+my_list = [4, 8, 2, 6, 3]
+
+my_set = {5, 2, 7}
+
+my_dict = {'a': 1, 'b': 2, 'c': 10}
+
+my_tuple = (8, 1, 5, 2)
+
+my_range = range(3, 10)
+
+my_str = "The string that dare not speak its name"
+
+my_zip = zip(my_list, range(5))
+
+my_generator = (x**2 for x in range(6))
